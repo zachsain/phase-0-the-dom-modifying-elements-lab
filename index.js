1bb518ad726674d956546e5ba2eef1b8f744e272
Zachary Sain <zachsain@gmail.com>
2021-09-08 12:27:51 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+const main = document.querySelector('#main');
+
+main.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.setAttribute('id', 'victory');
+
+
+document.body.appendChild(newHeader);
+
+newHeader. innerHTML = ("Zachary is the champion");
+
+
